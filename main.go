3746ba6182a600d5fa3f6c5641f12192c2b47871
Arne Jørgensen <arne@arnested.dk>
2018-10-24 21:12:58 +0200

    Refactor into simpler functions

--- main.go
+++ main.go
@@ -48,22 +48,31 @@ func main() {
 }
 
 func containerHealthy(containerID string, cli *client.Client, failOnUnhealthy *bool) (bool, error) {
-	info, err := cli.ContainerInspect(context.Background(), containerID)
+	healthStatus, err := containerHealthStatus(containerID, cli)
 	if err != nil {
 		return false, err
 	}
 
-	if info.State.Health == nil {
-		return true, nil
-	}
-
-	if *failOnUnhealthy == true && info.State.Health.Status == "unhealthy" {
+	if *failOnUnhealthy == true && healthStatus == "unhealthy" {
 		return false, errors.Errorf("%s is unhealthy", containerID)
 	}
 
-	if info.State.Health.Status == "healthy" {
+	if healthStatus == "healthy" {
 		return true, nil
 	}
 
 	return false, nil
+}
+
+func containerHealthStatus(containerID string, cli *client.Client) (string, error) {
+	info, err := cli.ContainerInspect(context.Background(), containerID)
+	if err != nil {
+		return "error", err
+	}
+
+	if info.State.Health == nil {
+		return "healthy", nil
+	}
+
+	return info.State.Health.Status, nil
 }
